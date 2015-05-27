5a1ff5439c483f71dbade5b0d88fabd457f912bc
Don Drake <ddrake@dreamingmind.com>
2015-05-27 13:09:12 -0700

    add comments and a automated debug output catcher

--- src/Lib/NameConventions.php
+++ src/Lib/NameConventions.php
@@ -2,6 +2,7 @@
 namespace App\Lib;
 
 use Cake\Core\ConventionsTrait;
+use Cake\Utility\Inflector;
 
 /**
  * Description of NameConventions
@@ -18,6 +19,12 @@ class NameConventions {
 		$this->_name = $name;
 	}
 	
+	/**
+	 * Magic access to the methods as properties
+	 * 
+	 * @param string $version The inflection version requested as a property name
+	 * @return string|NULL The inflected string or NULL
+	 */
 	public function __get($version) {
 		$method = "_$version";
 		if (method_exists($this, $method)) {
@@ -27,21 +34,73 @@ class NameConventions {
 		}
 	}
 	
-	protected function _name($name) {
-		return $name;
-	}
-	
+	/**
+	 * Magic output if the object is echoed
+	 * 
+	 * @return string
+	 */
 	public function __toString() {
 		return $this->_name;
 	}
 	
 	/**
+	 * var_dump output
+	 * 
+	 * The list of 'property' names and their values
+	 * 
+	 * @return array
+	 */
+	public function __debugInfo() {
+			$ar = [
+				'echo $thisObject' => $this->_name,
+				'protected properties' => [
+					"_name" => $this->_name 
+				],
+				'public properties'	=> []
+			];
+			$methods = get_class_methods($this);
+			foreach ($methods as $method) {
+				if (stristr($method, '__') || $method === '_name') {
+					continue;
+				}
+				$label = str_replace('_', '', $method);
+				$ar['public properties'][$label] = $this->$method($this->_name);
+			}
+			return $ar;
+	}
+	
+	/**
+	 * A convenience 'property'
+	 * 
+	 * If someone has the object on $alias and they ask for $alias->name this will fulfill the request
+	 * 
+	 * @param string $name
+	 * @return string
+	 */
+	protected function _name($name) {
+		return $name;
+	}
+	
+	/**
 	 * This one doesn't make sense in this class
+	 * 
+	 * But I've fixed it so it will return a valid result
 	 * 
 	 * @param string $key
 	 * @return NULL
 	 */
-	protected function _modelNameFromKey($key) {
-		return NULL;
+	protected function _modelNameFromKey($name) {
+        $key = str_replace('_id', '', $this->_modelKey($name));
+        return Inflector::camelize(Inflector::pluralize($key));
+	}
+	
+	/**
+	 * for a modelName request
+	 * 
+	 * @param string $key
+	 * @return string
+	 */
+	protected function _modelName($name) {
+        return $this->_modelNameFromKey($this->_modelKey($name));
 	}
 }
